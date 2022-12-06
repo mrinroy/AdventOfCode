f160ec61ddd8817347f48295c8499a1fc43bb814
Mrinal Roy <mrinroy@gmail.com>
2022-12-06 00:44:28 -0500

    Read input once

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,20 +1,19 @@
 
-def find_window(in_file, window):
-    with open(in_file) as file:
-        line = file.readline()
-        char_pos = [-1]*26 # since only lowercase chars
-        pos_dict = {}
-        start = 0
-        for end, c in enumerate(line):
-            ix = ord(c) - ord('a')
-            if char_pos[ix] != -1:
-                start = max(start,char_pos[ix] + 1)
-            char_pos[ix] = end
-            if end - start == window - 1:
-                return end + 1
-            
-        return -1
+def find_window(stream, window):
+    char_pos = [-1]*26 # since only lowercase chars
+    start = 0
+    for end, c in enumerate(stream):
+        ix = ord(c) - ord('a')
+        if char_pos[ix] != -1:
+            start = max(start,char_pos[ix] + 1)
+        char_pos[ix] = end
+        if end - start == window - 1:
+            return end + 1
+        
+    return -1
 
 if __name__ == '__main__':
-    print("part1: ",find_window("input.txt", 4))
-    print("part2: ",find_window("input.txt", 14))
+    with open("input.txt") as file:
+        line = file.readline()
+    print("part1: ",find_window(line, 4))
+    print("part2: ",find_window(line, 14))
