d9308971d22ebfd0190581e3b5fce98762bbd26c
Mrinal Roy <mrinroy@gmail.com>
2022-12-06 00:42:04 -0500

    O(1) space

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,12 +2,14 @@
 def find_window(in_file, window):
     with open(in_file) as file:
         line = file.readline()
+        char_pos = [-1]*26 # since only lowercase chars
         pos_dict = {}
         start = 0
         for end, c in enumerate(line):
-            if c in pos_dict:
-                start = max(start,pos_dict[c] + 1)
-            pos_dict[c] = end
+            ix = ord(c) - ord('a')
+            if char_pos[ix] != -1:
+                start = max(start,char_pos[ix] + 1)
+            char_pos[ix] = end
             if end - start == window - 1:
                 return end + 1
             
