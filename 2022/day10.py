a5c7a626ea800db1a81e05bd991564a01e6d2540
Mrinal Roy <mrinroy@gmail.com>
2022-12-11 12:42:12 -0500

    decouple?

--- 2022/day10.py
+++ 2022/day10.py
@@ -14,7 +14,7 @@ def simulate(input_file):
                 val += int(command[1])
     return cycle_val
 
-def part_1(cycle_val):
+def part_1(cycle_val, checks):
     ans = 0
     for check in checks:
         # if check is not there check - 1 has to be there
@@ -27,7 +27,7 @@ def get_pixel(pos, cycle_val):
     sprite_start = cycle_val.get(ix, cycle_val.get(ix-1)) - 1
     if sprite_start <= j <= sprite_start + 2:
         return '#'
-    return ' '
+    return ' ' # space renders better readability that '.'
 
 def part_2(cycle_val):
     grid = [['' for _ in range(40)] for _ in range(6)]
@@ -40,7 +40,8 @@ if __name__ == "__main__":
     checks = [20, 60, 100, 140, 180, 220]
 
     cycle_val = simulate("input.txt")
-    print(f"Part 1: {part_1(cycle_val)}")
+    
+    print(f"Part 1: {part_1(cycle_val, checks)}")
 
     grid = part_2(cycle_val)
     for row in grid:
